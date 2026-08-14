490e8f2ceae60d9045af8bfc212834587ca2f678
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 19:03:50 +0530

    Auto update: 2026-08-14 19:03:48

--- 08_2D_Arrays/Maximum_row_sum.cpp
+++ 08_2D_Arrays/Maximum_row_sum.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <climits>
 using namespace std;
 
 int main()
 {
-    // int arr[3][4];
-    int sum = 0;
-    int arr[][4] = {{5, 8, 1, 2}, {9, 9, 4, 4}, {7, 0, 3, 5}};
+    int maxrow = -1, maxsum = INT_MIN;
+
+    int arr[][4] = {
+        {5, 8, 1, 2},
+        {9, 9, 4, 4},
+        {7, 0, 3, 5}};
 
     for (int i = 0; i < 3; i++)
     {
+        int sum = 0;
+
         for (int j = 0; j < 4; j++)
         {
-            cout << arr[i][j] << " ";
             sum += arr[i][j];
         }
-        cout << endl;
+
+        if (sum > maxsum)
+        {
+            maxsum = sum;
+            maxrow = i;
+        }
     }
-    cout << sum << endl;
+
+    cout << maxrow << endl;
 }
