bd9e998d7e705d93dda9c32cee20adba16919469
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:11:41 +0530

    Auto update: 2026-08-14 18:11:41

--- 07_Arrays/Merge_tow_sorted.cpp
+++ 07_Arrays/Merge_tow_sorted.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int a[]= {10,20,40,70,90,100};
-    int b[]={30,50,60,80};
-    
+    int a[] = {10, 20, 40, 70, 90, 100};
+    int b[] = {30, 50, 60, 80};
 }
