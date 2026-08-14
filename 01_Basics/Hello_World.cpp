ce90f115b13d0eea49ea09cac02a93e785a0fa87
Tanmay sonwane <tanmaysonawane92@gmail.com>
2026-08-14 14:59:44 +0530

    Add Hello World program in C++

---
+++ 01_Basics/Hello_World.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int  main()
+{int a=10;
+    cout<<"hello world";
+    cout<<a;
+
+
+    cin>>a;
+    cout<<a;
+    for (int i=0;i<10;i++)
+    {
+        cout<<i;
+    }
+    
+
+}
