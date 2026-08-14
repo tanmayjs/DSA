be4dd464d638e390ce29eb43fc5a7c6180a75295
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:47:06 +0530

    Auto update: 2026-08-14 18:47:06

--- 08_2D_Arrays/syntax_of_2D.cpp
+++ 08_2D_Arrays/syntax_of_2D.cpp
@@ -6,7 +6,7 @@ int main()
 {   
     // int arr[3][4];
     int arr[][4] = {{5,8,1,2},{9,9,4,4},{7,0,3,5}};
-    for(i=0;i<3;i++){
+    for(int i=0;i<3;i++){
         for(int j = 0;j<4;j++){
             cout<<arr[i][j]<< " ";
         }
