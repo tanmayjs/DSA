f6602f561e925c97586ebe6f2803a744b1fa0f99
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:55:59 +0530

    Auto update: 2026-08-14 18:55:59

--- 08_2D_Arrays/Maximum_row_sum.cpp
+++ 08_2D_Arrays/Maximum_row_sum.cpp
@@ -13,16 +13,9 @@ int main()
         for (int j = 0; j < 4; j++)
         {
             cout << arr[i][j] << " ";
+            
         }
         cout << endl;
     }
 }
 
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 4; j++)
-    {
-        cout << arr[i][j] << " ";
-    }
-    cout << endl;
-}
