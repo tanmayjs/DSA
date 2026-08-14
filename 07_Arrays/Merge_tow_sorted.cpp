3e7cc83cd8ca51af9a98438082fd0c80cefb6684
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:12:36 +0530

    Auto update: 2026-08-14 18:12:36

--- 07_Arrays/Merge_tow_sorted.cpp
+++ 07_Arrays/Merge_tow_sorted.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int a[] = {10, 20, 40, 70, 90, 100};
     int b[] = {30, 50, 60, 80};
-    int m = sizeof(a)/4;
-    int n = sizeof(b)/4;
-    int c[m+n] 
+    int m = sizeof(a) / 4;
+    int n = sizeof(b) / 4;
+    int c[m + n];
 }
