5fd312a2a5eb3083df45f55d5406e0251454a730
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:55:35 +0530

    Auto update: 2026-08-14 18:55:35

--- 08_2D_Arrays/Maximum_row_sum.cpp
+++ 08_2D_Arrays/Maximum_row_sum.cpp
@@ -8,15 +8,14 @@ int main()
     int sum =0;
     int arr[][4] = {{5,8,1,2},{9,9,4,4},{7,0,3,5}};
     
-    for(int j = 0;j<4;j++){
-        for(int i=0;i<3;i++){
-            // cout<<arr[i][j]<< " ";
-            sum += arr[i][j];
-        }
-        // cout<<endl;
-        
-    }
-    cout<<sum;
+    /     for(int i=0;i<3;i++){
+//         for(int j = 0;j<4;j++){
+//             cout<<arr[i][j]<< " ";
+//         }
+//         cout<<endl;
+//     }
+// 
+    
 }
 
 //     for(int i=0;i<3;i++){
