75711eda59af5be9009dde06b145720fbcf86451
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:53:09 +0530

    Auto update: 2026-08-14 18:53:08

--- 08_2D_Arrays/syntax_of_2D.cpp
+++ 08_2D_Arrays/syntax_of_2D.cpp
@@ -5,11 +5,24 @@ using namespace std;
 int main()
 {   
     // int arr[3][4];
+    int sum =0;
     int arr[][4] = {{5,8,1,2},{9,9,4,4},{7,0,3,5}};
-    for(int i=0;i<3;i++){
-        for(int j = 0;j<4;j++){
-            cout<<arr[i][j]<< " ";
+    
+    for(int j = 0;j<4;j++){
+        for(int i=0;i<3;i++){
+            // cout<<arr[i][j]<< " ";
+            sum += arr[i][j];
         }
-        cout<<endl;
+        // cout<<endl;
+        
     }
+    cout<<sum;
 }
+
+//     for(int i=0;i<3;i++){
+//         for(int j = 0;j<4;j++){
+//             cout<<arr[i][j]<< " ";
+//         }
+//         cout<<endl;
+//     }
+// 
