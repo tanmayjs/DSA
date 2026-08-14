6c9f5e3b5176a10e4b2590d419d8d51d565d7e09
Tanmay sonwane <tanmaysonawane92@gmail.com>
2026-08-14 15:00:35 +0530

    Add files via upload

---
+++ 01_Basics/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout<<"Enter first number: ";
+    cin>>n;
+    cout<<"Enter second number: ";
+    cin>>m;
+    cout<<"The sum of "<<n<<" and "<<m<<" is: ";
+    cout<<n+m;
+}
---
+++ 01_Basics/simpleintrest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    float p, r, t;
+    cout<<"Enter principal amount: ";
+    cin>>p;
+    cout<<"Enter rate of interest: ";
+    cin>>r;
+    cout<<"Enter time in years: ";
+    cin>>t;
+    cout<<"Simple Interest: "<<(p*r*t)/100;
+    
+
+}
