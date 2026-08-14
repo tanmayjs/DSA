ada8a3d2eb1d6aba5c255c72a56275600386ed89
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:23:57 +0530

    Auto update: 2026-08-14 18:23:57

--- 07_Arrays/Merge_two_sorted.cpp
+++ 07_Arrays/Merge_two_sorted.cpp
@@ -24,11 +24,10 @@ int main()
         while (j < n)
             c[k++] = b[j++];
     
-    else
-    {
+   
         while (j < m)
             c[k++] = a[i++];
-    }
+    
     for (int i = 0; i < m + n; i++)
     {
         cout << c[i] << " ";
