a03dc95471288408e2361a8b757e883c7008e45c
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:34:48 +0530

    Auto update: 2026-08-14 18:34:48

--- 07_Arrays/Merge_two_sorted.cpp
+++ 07_Arrays/Merge_two_sorted.cpp
@@ -6,29 +6,31 @@ int main()
 {
     int a[] = {10, 20, 40, 70, 90, 100};
     int b[] = {30, 50, 60, 80};
+
     int m = sizeof(a) / 4;
     int n = sizeof(b) / 4;
+
     int c[m + n];
+
     int i = 0, j = 0, k = 0;
+
     while (i < m && j < n)
     {
         if (a[i] < b[j])
-
             c[k++] = a[i++];
-
         else
-            // b[j]< a[i]
             c[k++] = b[j++];
     }
 
     while (j < n)
         c[k++] = b[j++];
 
-    while (j < m)
+    while (i < m)
         c[k++] = a[i++];
 
     for (int i = 0; i < m + n; i++)
     {
         cout << c[i] << " ";
     }
+    // H
 }
