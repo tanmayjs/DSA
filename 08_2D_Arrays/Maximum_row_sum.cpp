66d7768d275895f4e33a19b7a7624f80724db32c
tanmayjs <tanmaysonawane92@gmail.com>
2026-08-14 18:55:47 +0530

    Auto update: 2026-08-14 18:55:47

--- 08_2D_Arrays/Maximum_row_sum.cpp
+++ 08_2D_Arrays/Maximum_row_sum.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main()
-{   
+{
     // int arr[3][4];
-    int sum =0;
-    int arr[][4] = {{5,8,1,2},{9,9,4,4},{7,0,3,5}};
-    
-    /     for(int i=0;i<3;i++){
-//         for(int j = 0;j<4;j++){
-//             cout<<arr[i][j]<< " ";
-//         }
-//         cout<<endl;
-//     }
-// 
-    
+    int sum = 0;
+    int arr[][4] = {{5, 8, 1, 2}, {9, 9, 4, 4}, {7, 0, 3, 5}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
-//     for(int i=0;i<3;i++){
-//         for(int j = 0;j<4;j++){
-//             cout<<arr[i][j]<< " ";
-//         }
-//         cout<<endl;
-//     }
-// 
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+}
