644d2f188f145e47ecde6dd3909288c70fb72b9b
Tanmay sonwane <tanmaysonawane92@gmail.com>
2026-08-14 15:39:06 +0530

    Create 0066_Plus_one.cpp

---
+++ 31_LeetCode/Arrays/0066_Plus_one.cpp
@@ -0,0 +1,17 @@
+// 66. Plus One
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry =1;
+        vector<int> ans;
+        for(int i = )
+    }
+};
