d65aa332380c6aa4d30703cf9d38c605c6ae0433
Tanmay sonwane <tanmaysonawane92@gmail.com>
2026-08-15 15:07:29 +0530

    Create Multiply_Two_Matrices.cpp

---
+++ 32_GeeksforGeeks/Arrays/Multiply_Two_Matrices.cpp
@@ -0,0 +1,20 @@
+// Multiply Two Matrices
+// Difficulty: BasicAccuracy: 46.94%Submissions: 31K+Points: 1
+
+
+class Solution {
+	public:
+	vector<vector<int>> multiplyMatrices(vector<vector<int>> &a, vector<vector<int>> &b) {
+		int n = a.size();
+		vector<vector<int>> res(n, vector<int>(n, 0));
+		for (int i = 0; i<n; i++) {
+			for (int j = 0; j<n; j++) {
+				// resu[i][j] = summation(k->0 to n-1) a[i][k]*b[k][j]
+				for (int k = 0; k<n; k++) {
+					res[i][j] += a[i][k]*b[k][j];
+				}
+			}
+		}
+		return res;
+	}
+};
